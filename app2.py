49958a6d1fca5b01755be96523039b967b42b345
YACINE 105 <114157775+YACIINE105@users.noreply.github.com>
2025-04-18 16:36:40 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -200,7 +200,7 @@ model_name = "deepseek-llm"
 # Set Ollama base URL
 ollama_base_url = st.sidebar.text_input(
     "Ollama API URL",
-    value="https://e50a-156-199-172-163.ngrok-free.app",
+    value="https://1184-156-199-172-163.ngrok-free.app",
     help="Enter the URL where Ollama is running"
 )
 
